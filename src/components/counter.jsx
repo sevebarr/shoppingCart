2a1e3b3c533e6c8c2dc3ffc34ea864528a1509c2
sevebarr <seve.barrameda@gmail.com>
2020-05-11 14:32:56 +0800

    Added delete button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,75 +1,73 @@
-import React, { Component } from "react";
-import "./counters.css";
+import React, { Component } from 'react';
+import './counters.css';
 
 class Counter extends Component {
-  state = {
-    // controlled components don't have their own state
-    // // value: this.props.counter.value,
-    // props.counter because on parent component counter={counter}
-    //  // this is a local state -- no single source of truth
-    classes: {},
-    buttonClasses: "btn btn-secondary btn-lg m-2"
-  };
+	state = {
+		// controlled components don't have their own state
+		// // value: this.props.counter.value,
+		// props.counter because on parent component counter={counter}
+		//  // this is a local state -- no single source of truth
+		classes: {},
+		buttonClasses: 'btn btn-secondary btn-lg m-2'
+	};
 
-  // constructor() {
-  //   super();
-  //   this.handleIncrement = this.handleIncrement.bind(this);
-  // }
+	// constructor() {
+	//   super();
+	//   this.handleIncrement = this.handleIncrement.bind(this);
+	// }
 
-  render() {
-    // PROPS are the attributes that were passed down from the parent compenent
+	render() {
+		// PROPS are the attributes that were passed down from the parent compenent
 
-    return (
-      <div>
-        {this.props.children}
-        {/* this will render the CHILD elements within the parent compenent tags */}
-        <span className={this.getBadgeClasses()}>
-          <h2 className="numberButton">{this.props.counter.value}</h2>
-        </span>
-        {/* calling methods with brackets - object.method() - will result in context of object */}
-        <button
-          // onClick={this.handleIncrement}
-          onClick={() => this.props.onIncrement(this.props.counter)}
-          className={this.state.buttonClasses}
-        >
-          {/* calling method without brackets automatically results in context of window, must provide context */}
-          +
-        </button>
-        <button
-          onClick={() => this.props.onDecrement(this.props.counter)}
-          className={this.state.buttonClasses}
-        >
-          -
-        </button>
-        <button
-          onClick={() => this.props.onDelete(this.props.counter.id)}
-          className="button btn-danger btn-sm m-2"
-        >
-          DELETE
-        </button>
-      </div>
-    );
-  }
+		return (
+			<div>
+				{/* {this.props.children} */}
+				{/* this will render the CHILD elements within the parent compenent tags */}
+				<span className={this.getBadgeClasses()}>
+					<h2 className="numberButton">{this.props.counter.value}</h2>
+				</span>
+				{/* calling methods with brackets - object.method() - will result in context of object */}
+				<button
+					// onClick={this.handleIncrement}
+					onClick={() => this.props.onIncrement(this.props.counter)}
+					className={this.state.buttonClasses}
+				>
+					{/* calling method without brackets automatically results in context of window, must provide context */}
+					+
+				</button>
+				<button onClick={() => this.props.onDecrement(this.props.counter)} className={this.state.buttonClasses}>
+					-
+				</button>
+				<button
+        // on click, pass this.props.counter.id to the onDelete method
+					onClick={() => this.props.onDelete(this.props.counter.id)}
+					className="button btn-danger btn-sm m-2"
+				>
+					DELETE
+				</button>
+			</div>
+		);
+	}
 
-  // handleIncrement = () => {
-  //   this.setState({ value: this.state.value + 1 });
-  // }; REMOVED to refactor to controlled compoenent
+	// handleIncrement = () => {
+	//   this.setState({ value: this.state.value + 1 });
+	// }; REMOVED to refactor to controlled compoenent
 
-  // handleDecrement = () => {
-  //   this.state.value !== 0
-  //     ? this.setState({ value: this.props.counter.value - 1 })
-  //     : this.setState({ value: this.props.counter.value });
-  // }; REMOVED to refactor to controlled compoenent
+	// handleDecrement = () => {
+	//   this.state.value !== 0
+	//     ? this.setState({ value: this.props.counter.value - 1 })
+	//     : this.setState({ value: this.props.counter.value });
+	// }; REMOVED to refactor to controlled compoenent
 
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.props.counter.value === 0 ? "warning" : "primary";
-    return classes;
-  }
-  // formatCount() {
-  //   const value = this.props.counter.value;
-  //   return value === 0 ? <h2>0</h2> : <h2>{value}</h2>;
-  // }
+	getBadgeClasses() {
+		let classes = 'badge m-2 badge-';
+		classes += this.props.counter.value === 0 ? 'warning' : 'primary';
+		return classes;
+	}
+	// formatCount() {
+	//   const value = this.props.counter.value;
+	//   return value === 0 ? <h2>0</h2> : <h2>{value}</h2>;
+	// }
 }
 
 export default Counter;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,85 +1,88 @@
-import React, { Component } from "react";
-import Counter from "./counter";
+import React, { Component } from 'react';
+import Counter from './counter';
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 999 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
-  };
+	state = {
+		counters: [ { id: 1, value: 999 }, { id: 2, value: 0 }, { id: 3, value: 0 }, { id: 4, value: 0 } ],
+		totalCounters: 4 // TO DO how do i get this to justbe the amount of counters?
+	};
 
-  handleReset = () => {
-    const counters = this.state.counters.map(c => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({ counters });
-  };
+	// takes all exisitng counters, maps it to c, sets all values to 0, not sure why it returns, then setsState of counters(component state) to counters(function variable)
+	handleReset = () => {
+		const counters = this.state.counters.map((c) => {
+			c.value = 0;
+			return c;
+		});
+		this.setState({ counters });
+	};
 
-  handleIncremenet = counter => {
-    const counters = [...this.state.counters];
-    // copies array into counters variable
-    const index = counters.indexOf(counter);
-    // checks the index of counter parm in new array and stores in index variable
-    //counters[index] = { ...counter };
-    //set the matching counter object in the new array to the object parm, I'M NOT SURE WHY THIS IS NEEDED
-    counters[index].value++;
-    // add one to the replaced object
-    this.setState({ counters });
-    //sets coutners of this state to new counters array
-  };
+	handleIncremenet = (counter) => {
+		// copies array into counters variable
+		const counters = [ ...this.state.counters ];
+		// gets the index of counter (subcomponent) in counters (state) being worked
+		const index = counters.indexOf(counter);
+		// increment this counter.value in new array by one
+		counters[index].value++;
+		// set state of the counters in Counters component to counters constant
+		this.setState({ counters });
+		console.log('increment', counter.value);
+	};
 
-  handleDecrement = counter => {
-    const newDecArray = [...this.state.counters];
-    const index = newDecArray.indexOf(counter);
-    newDecArray[index].value === 0
-      ? (newDecArray[index].value = 0)
-      : newDecArray[index].value--;
-    this.setState({ counters: newDecArray });
-    console.log("decrement");
-  };
+	handleDecrement = (counter) => {
+		const counters = [ ...this.state.counters ];
+		const index = counters.indexOf(counter);
+		counters[index].value === 0 ? (counters[index].value = 0) : counters[index].value--;
+		this.setState({ counters: counters });
+		console.log('decrement');
+	};
 
-  handleDelete = counterId => {
-    // console.log("Delete has been clicked", counterId);
-    const newCounters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({ counters: newCounters });
-    console.log(...newCounters, "delete");
-  };
+	handleDelete = (counterId) => {
+		// filter state.counters where !== counterId
+		const newCounters = this.state.counters.filter((c) => c.id !== counterId);
+		this.setState({ counters: newCounters });
+		console.log(...newCounters, 'delete');
+	};
 
-  render() {
-    return (
-      <div>
-        <button
-          onClick={this.handleReset}
-          className="btn btn-primary btn-sm m-2"
-        >
-          Reset
-        </button>
-        {this.state.counters.map(counter => (
-          <Counter
-            key={counter.id}
-            // key is used internally by react so we won't be able to access it in the Component, that's why id is repeated with an identical value
-            counter={counter} // replace indivdual assignments
-            //id={counter.id}
-            //value={counter.value}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncremenet}
-            onDecrement={this.handleDecrement}
-            // These are the PROPS that are passed from parent compenent to child component, ie. these will be avaialble in Counter(child) component
-            // can also send the following to avoid individually assigning attributes down to component:
-            //counter={counter}
-          >
-            <h2>#{counter.id}</h2>
-            {/* // Anything between the tags such as childe elements can be passed
+	handleAddCounter = (counter) => {
+		const myCounters = [ ...this.state.counters ];
+		let newIdx = this.state.totalCounters + 1;
+		myCounters.push({ id: newIdx, value: 0 });
+		this.setState({ totalCounters: this.state.totalCounters + 1, counters: myCounters });
+		console.log(this.state.totalCounters);
+	};
+
+	render() {
+		return (
+			<div>
+				<button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">
+					Reset Quantity
+				</button>
+				<button onClick={this.handleAddCounter} className="btn btn-primary btn-sm m-2">
+					Add Counter
+				</button>
+				{/* for each counter, render counter object */}
+				{this.state.counters.map((counter) => (
+					<Counter
+						key={counter.id}
+						// key is used internally by react so we won't be able to access it in the Component, that's why id is repeated with an identical value
+						counter={counter} // replace indivdual assignments
+						//id={counter.id}
+						//value={counter.value}
+						onDelete={this.handleDelete}
+						onIncrement={this.handleIncremenet}
+						onDecrement={this.handleDecrement}
+						// These are the PROPS that are passed from parent compenent to child component, ie. these will be avaialble in Counter(child) component
+						// can also send the following to avoid individually assigning attributes down to component:
+						//counter={counter}
+					>
+						{/* <h2>#{counter.id}</h2> */}
+						{/* // Anything between the tags such as childe elements can be passed
             down to the child element ass CHILDERN */}
-          </Counter>
-        ))}
-      </div>
-    );
-  }
+					</Counter>
+				))}
+			</div>
+		);
+	}
 }
 
 export default Counters;
